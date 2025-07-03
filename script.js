d9e8af302fbfa15b8df143a7fdfe288b72d60762
Daniel Lucena Alonso <daniellucena154@gmail.com>
2025-07-03 13:48:35 -0300

    modify html and css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function check() {
   if ( document.forms[0].elements[0].checked == true && document.forms[0].elements[1].checked == true && document.forms[0].elements[2].checked == true ) {
     if ( ! $('.wrapper').hasClass('throb')) {
